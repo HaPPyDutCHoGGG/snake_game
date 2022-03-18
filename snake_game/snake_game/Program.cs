b1ad27c420e3c8e5cc60ba58a50f7fea3f5872ba
HaPPyDutCHoGGG <42973503+HaPPyDutCHoGGG@users.noreply.github.com>
2022-03-18 20:52:51 +0300

    Coordinates in console

---
+++ snake_game/snake_game/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x_1 = 1;
+            int y_1 = 3;
+            char sym_1 = '*';
+
+            Draw(x_1,y_1,sym_1);
+
+         
+            
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
