423183d6c1810d6f29a53ea61d65286c3950af2a
HaPPyDutCHoGGG <42973503+HaPPyDutCHoGGG@users.noreply.github.com>
2022-03-18 22:06:33 +0300

    Draw and coord.
    implementation of  Draw_func and coord. as class

--- snake_game/snake_game/Program.cs
+++ snake_game/snake_game/Program.cs
@@ -10,20 +10,34 @@ namespace snake_game
     {
         static void Main(string[] args)
         {
+            /*
             int x_1 = 1;
             int y_1 = 3;
             char sym_1 = '*';
 
             Draw(x_1,y_1,sym_1);
+            */
 
-         
-            
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 2;
+            p2.y = 4;
+            p2.sym = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
+        /*
         static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+        */
     }
 }
